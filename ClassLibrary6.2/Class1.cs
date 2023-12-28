d5ef7acd51cdd9a32e2b19486ddee477334160df
Platinum908 <estnuntius@gmail.com>
2023-12-28 03:57:54 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary6.2/Class1.cs
@@ -0,0 +1,39 @@
+﻿
+namespace ClassLibrary6._2
+{
+    public class Class1
+    {
+        public static double Array(int i, int j)
+        {
+            return Math.Sqrt(i - j) * Math.Pow(Math.Pow(Math.Cos(i), 3) + (Math.Pow(Math.Cos(j), 3)), 3) - Math.Pow(-9, i + j) * Math.Log(Math.Exp(i + 9));
+        }
+        public static double Vector(double[,] mas)
+        {
+            for (int j =0; j < mas.GetLength(1); j++)
+            {
+                double min = 0;
+                for (int i = 0; i < mas.GetLength(0); i++)
+                {
+                    if (i == 0) min = mas[j, i];
+                    min = min > mas[j, i] && mas[j, i] != double.NaN ? mas[j,i] : min;
+                }
+                Console.WriteLine($"Вектор столбца {j+1}: {Math.Pow(min,2)}");
+            }
+            return 0;
+        }
+        public static double Function(double[,] mas)
+        {
+            double p = 1, s = 0;
+            for(int k = 2; k < 3; k++)
+            {
+                for (int j = 6; j > k; j--)
+                {
+                    s += Math.Pow(Math.Abs(j - Math.Exp(1)), 4);
+                }
+                p *= 3 * s;
+            }
+            return Math.Log(Math.Abs(p), 5);
+        }
+
+    }
+}
---
+++ lab6.2_idrisov/Program.cs
@@ -0,0 +1,17 @@
+﻿//Лабораторная 6.2 Средний 24
+
+using ClassLibrary6._2;
+int n =5;
+int m =5;
+double[,] mas = new double[n,m];
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        mas[i, j] = (Class1.Array(i, j));
+        Console.Write($"{Class1.Array(i,j):F2} ");
+    }
+    Console.WriteLine();
+}
+Class1.Vector(mas);
+Console.WriteLine($"G = {Class1.Function(mas)}");
